36bd25631eef1fd49096aee65102683fdca4b88f
MAbdallah11 <abdallahmac9@gmail.com>
2025-06-17 21:35:47 +0300

    new changes

--- otp_sms.py
+++ otp_sms.py
@@ -6,7 +6,35 @@ from config import TWILIO_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE
 def generate_otp(length=6):
     return ''.join(random.choices(string.digits, k=length))
 
-def send_sms_otp(phone_number, otp):
+# def send_sms_otp(phone_number, otp):
+#     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+#     message = f"Your decryption OTP is: {otp}"
+#     client.messages.create(to=phone_number, from_=TWILIO_PHONE, body=message)
+
+
+def send_sms_otp(phone_number: str) -> str:
+    service_sid = 'VAa4213003ee4d0bca3f44c002c21b28ed'
+
     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
-    message = f"Your decryption OTP is: {otp}"
-    client.messages.create(to=phone_number, from_=TWILIO_PHONE, body=message)
+
+    verification = client.verify \
+        .v2 \
+        .services(service_sid) \
+        .verifications \
+        .create(to=phone_number, channel='sms')
+
+    print(f"Verification SID for {phone_number}: {verification.sid}")
+    return verification.sid
+
+def sending(self):
+            # Set the numbers in international format
+            recipients = ["+254114942044"]
+            # Set your message
+            message = "Hey AT Ninja!"
+            # Set your shortCode or senderId
+            sender = "XXYYZZ"
+            try:
+                response = self.sms.send(message, recipients, sender)
+                print (response)
+            except Exception as e:
+                print (f'Houston, we have a problem: {e}')
