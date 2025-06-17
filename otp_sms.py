6b04163123d46c4d73c42706abf6148db27c8a62
victormusembi11 <victor.musembi11@gmail.com>
2025-06-17 21:57:20 +0300

    feat: Integrate Africa Stalking API for SMS OTP delivery and refactor Twilio configuration

--- otp_sms.py
+++ otp_sms.py
@@ -1,7 +1,8 @@
 from twilio.rest import Client
 import random
 import string
-from config import TWILIO_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE
+from config import TWILIO_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE, AFRICASTALKING_API_KEY, AFRICASTALKING_USERNAME
+import africastalking
 
 def generate_otp(length=6):
     return ''.join(random.choices(string.digits, k=length))
@@ -12,29 +13,30 @@ def generate_otp(length=6):
 #     client.messages.create(to=phone_number, from_=TWILIO_PHONE, body=message)
 
 
-def send_sms_otp(phone_number: str) -> str:
-    service_sid = 'VAa4213003ee4d0bca3f44c002c21b28ed'
+# def send_sms_otp(phone_number: str) -> str:
+#     service_sid = 'VAa4213003ee4d0bca3f44c002c21b28ed'
 
-    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+#     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-    verification = client.verify \
-        .v2 \
-        .services(service_sid) \
-        .verifications \
-        .create(to=phone_number, channel='sms')
+#     verification = client.verify \
+#         .v2 \
+#         .services(service_sid) \
+#         .verifications \
+#         .create(to=phone_number, channel='sms')
 
-    print(f"Verification SID for {phone_number}: {verification.sid}")
-    return verification.sid
+#     print(f"Verification SID for {phone_number}: {verification.sid}")
+#     return verification.sid
 
-def sending(self):
-            # Set the numbers in international format
-            recipients = ["+254114942044"]
-            # Set your message
-            message = "Hey AT Ninja!"
-            # Set your shortCode or senderId
-            sender = "XXYYZZ"
-            try:
-                response = self.sms.send(message, recipients, sender)
-                print (response)
-            except Exception as e:
-                print (f'Houston, we have a problem: {e}')
+
+africastalking.initialize(AFRICASTALKING_USERNAME, AFRICASTALKING_API_KEY)
+sms = africastalking.SMS
+
+def send_sms_otp(phone_number: str, otp: str):
+    recipients = [phone_number]
+    message = f"Your decryption OTP is: {otp}"
+    sender = "XXYYZZ"
+    try:
+        response = sms.send(message, recipients, sender)
+        print(response)
+    except Exception as e:
+        print(f'Houston, we have a problem: {e}')
